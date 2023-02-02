5d390d77e137fdf1a4c3bf5f5fef6b4931df1266
MaximilianoLutz <lutzmaximiliano@gmail.com>
2023-02-02 16:27:30 -0300

    Comienzo proyecto

---
+++ src/main/java/ar/com/llegolaslutz/chatapi/ChatApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.llegolaslutz.chatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApiApplication.class, args);
+	}
+
+}
